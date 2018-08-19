dc313e0cc889511cb9af720dcbb3a4591214714e
Greg Mefford <greg@gregmefford.com>
2018-08-19 15:11:01 -0400

    Return tagged errors on builtin setup failure

--- lib/usb_gadget/builtin.ex
+++ lib/usb_gadget/builtin.ex
@@ -17,7 +17,7 @@ defmodule USBGadget.Builtin do
       iex> USBGadget.enable_device("g")
       :ok
   """
-  def create_rndis_ecm_acm(device_name) do
+  def create_rndis_ecm_acm(name) do
     # * bcdUSB 0x200 means USB 2.0
     # * bDevice{Class,SubClass,Protocol} = {0xEF, 0x02, 0x01} means
     #   Interface Association Descriptor class
@@ -26,7 +26,7 @@ defmodule USBGadget.Builtin do
     #   so 0x0123 means 1.2.3
     # * These os_desc options tell Windows to use configuration 1
     # * strings/0x409 means EN-US.
-    create_device(device_name, %{
+    device_settings = %{
       "bcdUSB" => "0x0200",
       "bDeviceClass" => "0xEF",
       "bDeviceSubClass" => "0x02",
@@ -46,23 +46,19 @@ defmodule USBGadget.Builtin do
           "serialnumber" => ""
         }
       }
-    })
+    }
 
     # This sub_compatible_id tells Windows to use the RNDIS driver version that
     # actually works instead of a newer one that doesn't. It's not well-
     # documented, but it's commonly used for Linux-based USB Gadgets.
-    create_function(device_name, "rndis.usb0", %{
+    rndis_settings = %{
       "os_desc" => %{
         "interface.rndis" => %{
           "compatible_id" => "RNDIS",
           "sub_compatible_id" => "5162001"
         }
       }
-    })
-
-    create_function(device_name, "ecm.usb1", %{})
-
-    create_function(device_name, "acm.GS0")
+    }
 
     # * bmAttributes is a bitmap field:
     #   * bit 7 (MSB) is reserved in USB 2.0 and set to 1
@@ -70,7 +66,7 @@ defmodule USBGadget.Builtin do
     #   * bit 5 set means the device can wake the host from suspend
     #   * bits 4..0 are reserved and set to 0
     # * MaxPower is the max current the device will draw (in mA)
-    create_config(device_name, "c.1", %{
+    config1_settings = %{
       "bmAttributes" => "0xC0",
       "MaxPower" => "500",
       "strings" => %{
@@ -78,9 +74,20 @@ defmodule USBGadget.Builtin do
           "configuration" => "RNDIS and ECM Ethernet with ACM Serial"
         }
       }
-    })
+    }
 
-    link_functions(device_name, "c.1", ["rndis.usb0", "ecm.usb1", "acm.GS0"])
-    link_os_desc(device_name, "c.1")
+    function_list = ["rndis.usb0", "ecm.usb1", "acm.GS0"]
+
+    with {:create_device, :ok} <- {:create_device, create_device(name, device_settings)},
+         {:create_rndis, :ok} <- {:create_rndis, create_function(name, "rndis.usb0", rndis_settings)},
+         {:create_ecm, :ok} <- {:create_ecm, create_function(name, "ecm.usb1", %{})},
+         {:create_acm, :ok} <- {:create_acm, create_function(name, "acm.GS0")},
+         {:create_config, :ok} <- {:create_config, create_config(name, "c.1", config1_settings)},
+         {:link_functions, :ok} <- {:link_functions, link_functions(name, "c.1", function_list)},
+         {:link_os_desc, :ok} <- {:link_os_desc, link_os_desc(name, "c.1")} do
+      :ok
+    else
+      {failed_step, {:error, reason}} -> {:error, {failed_step, reason}}
+    end
   end
 end
